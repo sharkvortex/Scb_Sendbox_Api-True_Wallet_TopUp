afe43ef1a1aa57785901c4de6dacf234bc045fa2
PAYUT.T <payuttobunma.pt@gmail.com>
2025-09-12 23:18:58 +0700

    SQL.txt

--- Frontend/src/components/axios.jsx
+++ Frontend/src/components/axios.jsx
@@ -1,5 +1,3 @@
 import axios from 'axios';
-
 axios.defaults.withCredentials = true;
-
 export default axios;
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -15,6 +15,8 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
+    host: true,
+    allowedHosts:['.ngrok-free.app'],
   }
   
 })
